eda79eb83d4ddacf45a64cc04958a266aa55413b
Alluri Vamsi <vamsi1306@gmail.com>
2020-05-26 02:08:23 +0530

    01 adding stats screen (#3)
    * Added Models
    
    * Added Models and empty logic file
    
    Co-authored-by: VamsiKrishna-Alluri <61180237+VamsiKrishna-Alluri@users.noreply.github.com>

---
+++ app/src/main/java/com/example/criccards/Models/CurrentDisplayed.java
@@ -0,0 +1,11 @@
+package com.example.criccards.Models;
+
+public class CurrentDisplayed {
+    protected int CurrentRuns;
+    protected int CurrentWickets;
+    protected int PreviousTeamTotalRuns;
+    protected int CurrentOver;
+    protected int CurrentBallNumber;
+    protected int NumberOfOvers;
+
+}
---
+++ app/src/main/java/com/example/criccards/Models/Team.java
@@ -0,0 +1,10 @@
+package com.example.criccards.Models;
+
+import java.util.List;
+
+public class Team {
+
+    protected String TeamName;
+    protected String UmpireName;
+    protected List<Player> Players;
+}
